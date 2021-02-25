5dcbdbfd1673fad3e1dd322ccd8d91879167cc64
Masayoshi Iraha <urasho6.12@gmail.com>
2021-02-25 22:10:53 +0900

    commit

---
+++ Assets/Scripts/EnemyGroup.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyGroup : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other) 
+    {
+
+        var player = other.GetComponent<PlayerHealth>();
+
+        if (player != null) 
+        {
+            Debug.Log("当たった");
+        }
+        
+    }
+
+    /*
+    [SerializeField] Transform _Target;  // ターゲット(Player)
+    [SerializeField] float _GroupChaseRange = 10f;   // EnemyがPlayerに気づく距離
+
+    private float _DistanceTarget = Mathf.Infinity; // _TargetとEnemyの距離
+    private bool _GroupEnemyNoticed = false; // EnemyがPlayの位置に気いたらTrue
+    private NavMeshAgent _NavMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GetComponent<EnemyMovement>();
+        _NavMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroupDistanceToTarget();
+    }
+
+    public void GroupDistanceToTarget()
+    {
+        // ターゲットの位置
+        _DistanceTarget = Vector3.Distance(_Target.position, transform.position);
+
+        // TargetがEngageTarget内に入ったら移動開始
+        if (_GroupEnemyNoticed)
+        {
+            GroupEngageTarget();
+        }
+        else if (_DistanceTarget <= _GroupChaseRange)
+        {
+            _GroupEnemyNoticed = true;
+            //FindObjectOfType<EnemyMovement>();
+            Debug.Log("エネミーがプレイヤーに気づいた");
+            FindObjectOfType<EnemyMovement>().GoToTarget();
+
+        }
+
+    }
+
+    private void GroupEngageTarget()
+    {
+        //LookAtTarget();
+        if (_DistanceTarget >= _NavMeshAgent.stoppingDistance)
+        {
+            _NavMeshAgent.SetDestination(_Target.position);
+        }
+
+    }
+
+    // ギズモでChaseRangeを見えるように設定
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _GroupChaseRange);
+    }
+    */
+
+
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] Transform _TargetObject;  // ターゲット
+    [SerializeField] float _TurnSpeed = 5f;
+
+    public float _DistanceTarget = Mathf.Infinity; // _TargetObjectとEnemyの距離
+    bool _Noticed = false;
+
+    NavMeshAgent _NavMeshAgent;
+
+
+    void Start()
+    {
+
+        _NavMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //DistanceTarget();
+        //_DistanceTarget = Vector3.Distance(_TargetObject.position, transform.position);
+
+        _NavMeshAgent.SetDestination(_TargetObject.position);
+    }
+
+    public void GoToTarget() 
+    {
+
+        if (_Noticed == true) 
+        {
+            
+        }
+
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 Direction = (_TargetObject.position - transform.position).normalized;
+        Quaternion _LookRotation = Quaternion.LookRotation(new Vector3(Direction.x, 0, Direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, _LookRotation, Time.deltaTime * _TurnSpeed);
+    }
+
+
+}
